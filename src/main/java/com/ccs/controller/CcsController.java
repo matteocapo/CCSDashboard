7b51897fb0d127ff3d1ad8daaebd4e651d987b43
InattentiveCoder <InattentiveCoder@users.noreply.github.com>
2018-11-20 16:13:45 +0100

    Revert "Revert "Revert "Aggiunta la gestione per le date"""
    This reverts commit c0b39d7c92d8b8ee076e09dceb92550579ce02ad.

--- src/main/java/com/ccs/controller/CcsController.java
+++ src/main/java/com/ccs/controller/CcsController.java
@@ -1,46 +1,46 @@
 package com.ccs.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.ccs.util.Date;
-
 @Controller
 public class CcsController {
-
+	
 	@RequestMapping("/indexprova")
-	public ModelAndView homepageview(
-			@RequestParam(value = "datetimes", required = false, defaultValue = "World") String date) {
-
+	public ModelAndView homepageview(@RequestParam(value = "datetimes", required = false, defaultValue = "World") String date) {
+		
 		System.out.println("in controller");
-
-		if (!date.equals("World"))
+		
+		if(!date.equals("World"))
 			System.out.println(date);
-		Date.toMindSphereFormat(date);
-
+		
+		
+		
 		ModelAndView mv = new ModelAndView("indexprova");
-
+		
 		mv.addObject("domenica", "350");
-		mv.addObject("lunedì", "340");
+	    mv.addObject("lunedì", "340");
 
-		// oee
-		mv.addObject("oee", "80%");
-
-		// scarti e produzioni
-		mv.addObject("produzioni", "200562");
-		mv.addObject("scarti", "8000");
-
-		// velocità ultima ora
-
-		mv.addObject("min1", "255");
-		mv.addObject("min2", "230");
-		mv.addObject("min3", "200");
-		mv.addObject("min4", "201");
-		mv.addObject("min5", "320");
-		mv.addObject("min6", "350");
-
+	     
+	    //oee
+	    mv.addObject("oee","80%");
+	    
+	    //scarti e produzioni
+	    mv.addObject("produzioni", "200562");
+	    mv.addObject("scarti", "8000"); 
+	     
+	    //velocità ultima ora
+	         
+	    mv.addObject("min1", "255"); 
+	    mv.addObject("min2", "230"); 
+	    mv.addObject("min3", "200");
+	    mv.addObject("min4", "201"); 
+	    mv.addObject("min5", "320"); 
+	    mv.addObject("min6", "350");
+		
 		return mv;
 	}
 
