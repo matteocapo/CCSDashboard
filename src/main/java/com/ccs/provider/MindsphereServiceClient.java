aaa72804640381727201707f5cead48d79762d90
matteocapo <matteo.capodicasa@gmail.com>
2018-11-20 14:54:07 +0100

    inizializzazione progetto

---
+++ src/main/java/com/ccs/provider/MindsphereServiceClient.java
@@ -0,0 +1,16 @@
+package com.ccs.provider;
+
+public class MindsphereServiceClient {
+	
+	public int oeeMedia(String from, String to) {
+		
+		return 1;
+	}
+	
+	public int[] prodottiEScarti(String from, String to) {
+		int[] dati = new int[2];
+		
+		return dati;
+	}
+
+}
---
+++ src/main/java/com/ccs/util/Provider.java
@@ -0,0 +1,16 @@
+package com.ccs.util;
+
+public class Provider {
+	
+	private static final String MINDSPHERE_BASE_URL = "/api/iottimeseries/v3";
+	
+	//da definire in seguito
+	private static final String MINDSPHERE_URL = "https://eu1.mindsphere.com";
+	
+	public static String buidMindsphereRequestURL(String entity, String propertysetname, String from, String to, String limit, String select){
+		//da completare la struttura della chiamata
+		String url = MINDSPHERE_URL + MINDSPHERE_BASE_URL +"/"+ entity +"/"+ propertysetname; 
+		return url;
+	}
+
+}
